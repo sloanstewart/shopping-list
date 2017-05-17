e7a06ef5aa1fbbdbb6335dc63c0c5d644f1e3eac
Sloan <sloanstewartdev@gmail.com>
2017-05-17 00:16:40 -0400

    hey it works

--- app.js
+++ app.js
@@ -8,35 +8,62 @@ var state = {
 	items: [],
 };
 
-// State modification
-var addItem = function(state, item) {
-	state.items.push(item);
-};
-
-var toggleItem = function(state, item) {
-	this.closest('.shopping-item').remove();
-};
-
-var deleteItem = function(state, item) {
-	item.closest('li').remove();
-};
-
 // Render functions
-var renderList = function(state, element) {
+var renderList = function(state) {
 	var itemsHTML = state.items.map(function(item) {
 		return '<li>'
         + '<span class="shopping-item">' + item + '</span>'
         + '<div class="shopping-item-controls">'
         + '<button class="shopping-item-toggle">'
-        +    '<span class="button-label">check</span>'
-        +  '</button>'
+        +    '<span class="button-label">check</span> '
+        +  '</button> ' //had to cheat a space here
         +  '<button class="shopping-item-delete">'
         +    '<span class="button-label">delete</span>'
         +  '</button>'
         + '</div>'
         + '</li>';
 	});
-	element.append(itemsHTML);
+	$('.shopping-list').html(itemsHTML);
+
+	$('.shopping-item-toggle').click(function(event) {
+		event.preventDefault();
+		toggleItem(this);
+	});
+
+
+	$('.shopping-item-delete').click(function(event) {
+		event.preventDefault();
+		deleteItem(state, this);
+	});
+
+	console.log('Items: ' + state.items)
+};
+
+// State modification
+var addItem = function(state, item) {
+	state.items.push(item);
+	console.log('Added: ' + item);
+};
+
+var addExistingItem = 
+	$('.shopping-item').each(function() {
+		addItem(state, $(this).text() );
+		renderList(state);
+	});
+
+var toggleItem = function(item) {
+	var checkThis = $(item).closest('li').find('.shopping-item');
+	checkThis.toggleClass('shopping-item__checked');
+	console.log('Toggled: ' + $(checkThis).text());
+};
+
+var deleteItem = function(state, item) {
+	var deleteThis = $(item).closest('li').find('.shopping-item').text();
+	state.items = state.items.filter(function(x) {
+		return deleteThis !== x;
+	});
+	console.log('Deleted: ' + deleteThis);
+	renderList(state);
 };
 
 // Event listeners
@@ -44,16 +71,7 @@ $('#js-shopping-list-form').submit(function(event) {
 	event.preventDefault();
 	addItem(state, $('#shopping-list-entry').val());
 	// alert('entered: ' + $('#shopping-list-entry').val());
-	renderList(state, $('.shopping-list'));
+	renderList(state);
 });
 
-$('.shopping-item-toggle').click(function(event) {
-	event.preventDefault();
-	toggleItem(state, this);
-});
-
-
-$('.shopping-item-delete').click(function(event) {
-	event.preventDefault();
-	deleteItem(state, this);
-});
+addExistingItem;	
